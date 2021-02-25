d59fc12b55a810f5dc4788917df678418ae238b5
mbelduque <m.belduque@hotmail.com>
2021-02-25 02:52:53 -0500

    tasks dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,35 @@ function App() {
     <div className="container mt-5">
       <h1>Tareas</h1>
       <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Nombre de la tarea</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">
+                Eliminar
+              </button>
+              <button className="btn btn-warning btn-sm float-right">
+                Editar
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese la tarea..."
+            ></input>
+            <button className="btn btn-dark btn-block" type="submit">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
